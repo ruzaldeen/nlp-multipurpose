285e499ef16f237e0155a2d3a1642b8295cb663d
Ruzaldeen Zainal Abidin <ruzaldeen@gmail.com>
2021-08-09 16:37:32 +0800

    Added main.py for frontend testing

--- src/config.py
+++ src/config.py
@@ -6,3 +6,18 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt, seaborn as sns
 import streamlit as st
+
+class Config:
+
+    hide_streamlit_style = '''
+			<style>
+            footer {visibility: hidden;}
+            footer:after {
+            content:'_____________________________Developed and Deployed by Ruzaldeen Zainal Abidin________________________________';
+            visibility: visible;
+            display: block;
+            position: relative;
+            }
+            </style>
+            
+            '''
---
+++ src/main.py
@@ -0,0 +1,9 @@
+# Author: Ruzaldeen Zainal Abidin
+
+from config import *
+
+st.set_page_config(layout="wide")
+
+st.markdown("<h1 style='text-align: center; color: black;'>Multipurpose Natural Language Processing App</h1>", 
+            unsafe_allow_html=True)
+st.markdown(Config.hide_streamlit_style, unsafe_allow_html=True)
